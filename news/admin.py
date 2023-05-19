af88a4c36052e7fdd7ca6c6c51504597b61c6afd
ViktorSanych <i_vitek@mail.ru>
2023-05-19 14:24:01 +0300

    Финиш модуля D5.

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment
 
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(PostCategory)
-admin.site.register(Comment)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostCategoryInline(admin.TabularInline):
+    model = PostCategory
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+    inlines = [
+        PostCategoryInline,
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
