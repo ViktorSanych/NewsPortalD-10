a98fed0231ef9f55d5348496a0075db4309bd54c
ViktorSanych <i_vitek@mail.ru>
2023-06-06 17:17:03 +0300

    Настройка внешнего вида главной страницы

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
